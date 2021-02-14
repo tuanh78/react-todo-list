2c17cb5bd80f969a2805cdf1d33702cabe47fe85
tuanh78 <phamtuanh2000@gmail.com>
2021-02-14 09:01:12 +0700

    test vscode

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import uuid from "uuid";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TodoList from "./components/TodoList";
 import TodoInput from "./components/TodoInput";
+// test vs code
 export default class App extends Component {
   render() {
     return (
